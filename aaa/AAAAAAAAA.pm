38b712417f80c08bf9d0ba326e850a3b1b51fe70
Michael G Schwern <mschwern@cpan.org>
2011-07-27 17:17:30 +0000

    Import of MSCHWERN/AAAAAAAAA-1.01 from CPAN.
    gitpan-cpan-distribution: AAAAAAAAA
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         MSCHWERN/AAAAAAAAA-1.01.tar.gz
    gitpan-cpan-author:       MSCHWERN
    gitpan-cpan-maturity:     released

--- aaa/AAAAAAAAA.pm
+++ aaa/AAAAAAAAA.pm
@@ -3,25 +3,25 @@ package AAAAAAAAA;
 use strict;
 use warnings;
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 my @aaaaaaa = ('a'..'z', 'A'..'Z', 0..9);
-my %aaa_aaa;
+my %aaaaaaaa_aa_aaaa;
 for my $a (0..$#aaaaaaa) {
     my $aaaa = sprintf("%06b", $a);
     $aaaa =~ s{0}{a}g;
     $aaaa =~ s{1}{A}g;
-    $aaa_aaa{ $aaaaaaa[$a] } = $aaaa;
+    $aaaaaaaa_aa_aaaa{ $aaaaaaa[$a] } = $aaaa;
 }
-my %aaaaaaaa_aa_aaaa = map { $_ => $aaa_aaa{$_} } @aaaaaaa;
+
 my %aaaa_aa_aaaaaaaa;
 @aaaa_aa_aaaaaaaa{values %aaaaaaaa_aa_aaaa} = keys %aaaaaaaa_aa_aaaa;
 
-
 sub aaaa {
-    open my $aa, $0 or die "Aaa'a aaaa aaa aaaaaa aaaa aaa aaaaaaaaaaa: $!";
+    open my $aa, "<", $0 or die "Aaa'a aaaa aaa aaaaaa aaaa aaa aaaaaaaaaaa: $!";
+
     my $aaaa = join "", <$aa>;
-    $aaaa =~ s{use\s+AAAAAAAAA\b.*\n}{};
+    $aaaa =~ s{use\s+AAAAAAAAA\b}{}x;
 
     # Aaa aaa aaaaaaa
     if( $aaaa =~ /[b-zB-Z0-9]/ ) {
@@ -40,10 +40,10 @@ sub aaaa {
 sub aaaaaa {
     my $aaaa = shift;
 
-    $$aaaa =~ s{([a-zA-Z0-9])}{ $aaaaaaaa_aa_aaaa{$1} }gex;
+    $$aaaa =~ s{([a-zA-Z0-9])}{$aaaaaaaa_aa_aaaa{$1}}gx;
 
-    open my $aa, ">$0" or die "Aaa'a aaaa aaa aaaaaa aaaa aaa aaaaaaaaaaa: $!";
-    print $aa "use AAAAAAAAA;\n";
+    open my $aa, ">", $0 or die "Aaa'a aaaa aaa aaaaaa aaaa aaa aaaaaaaaaaa: $!";
+    print $aa "use AAAAAAAAA";
     print $aa $$aaaa;
 
     return;
@@ -53,7 +53,7 @@ sub aaaaaa {
 sub aaaaaaaa {
     my $aaaa = shift;
 
-    $$aaaa =~ s{ ([Aa]{6}) }{ $aaaa_aa_aaaaaaaa{$1} }gex;
+    $$aaaa =~ s{ ([Aa]{6}) }{$aaaa_aa_aaaaaaaa{$1}}gx;
 
     return;
 }
